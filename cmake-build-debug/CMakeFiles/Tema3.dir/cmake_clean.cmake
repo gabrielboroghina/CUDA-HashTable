65966322cbbb8d25bdce6dd0178215c95de71c3d
Gabriel Boroghina <gabrielboroghina@outlook.com>
2019-05-02 16:28:37 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Tema3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema3.dir/std_hashtable.cpp.o"
+  "CMakeFiles/Tema3.dir/test_map.cpp.o"
+  "Tema3.pdb"
+  "Tema3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
